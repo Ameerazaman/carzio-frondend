4a9099fb782dbb65a80a0e17a2926787c870c5a8
Fathimath Ameera <fathimathameeraap2gmail.com>
2025-01-14 08:20:08 +0530

    fixing error ins earching

--- frondend/src/Services/Socket.ts
+++ frondend/src/Services/Socket.ts
@@ -1,4 +1,4 @@
-import { io, Socket } from "socket.io-client";
+ import { io, Socket } from "socket.io-client";
 
 const socket: Socket = io("http://localhost:5000", {
   transports: ["websocket"], // Enforce websocket connection for better real-time experience
